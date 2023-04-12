3fefb5f551b80d518a6c8a8773be7208d1e3a5e0
tusharjayanti <tusharjayanti@gmail.com>
2023-04-13 04:16:26 +0530

    add problem #26

---
+++ 26. Remove Duplicates from Sorted Array/Solution.java
@@ -0,0 +1,23 @@
+//  Author: Tushar Jayanti
+//  github: https://github.com/tusharjayanti
+// LinkedIn: https://www.linkedin.com/in/tusharjayanti/
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], (map.get(nums[i]) + 1));
+                continue;
+            }
+            map.put(nums[i], 1);
+        }
+        int len = 0;
+        for (Map.Entry e : map.entrySet()) {
+            nums[len] = (int) e.getKey();
+            len++;
+        }
+
+        return len;
+    }
+}
