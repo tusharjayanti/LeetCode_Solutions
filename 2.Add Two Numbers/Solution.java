0a02c3455d4fd7f89adc355d47dc65c04c5a01ac
tusharjayanti <tusharjayanti@gmail.com>
2023-04-25 17:38:21 +0530

    add problem #2

--- 2.Add Two Numbers/Solution.java
+++ 2.Add Two Numbers/Solution.java
@@ -2,36 +2,45 @@
 //  github: https://github.com/tusharjayanti
 // LinkedIn: https://www.linkedin.com/in/tusharjayanti/
 
+// Runtime: 2 ms, beats 98.98% of Java online submissions 
+// Memory Usage: 42.9 MB beats 32.14% of Java online submissions 
+
 /*
-You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. 
-Add the two numbers and return the sum as a linked list.
-You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-Input: l1 = [2,4,3], l2 = [5,6,4]
-Output: [7,0,8]
-Explanation: 342 + 465 = 807.
-Input: l1 = [0], l2 = [0]
-Output: [0]
-Example 3:
-
-Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
-Output: [8,9,9,9,0,0,0,1]
-
-*/
-
-/**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-       while()
+        if (l1 == null && l2 == null)
+            return null;
+        ListNode result = new ListNode(0);
+        ListNode current = result;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int a = 0, b = 0, sum = 0;
+            if (l1 != null) {
+                a = l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                b = l2.val;
+                l2 = l2.next;
+            }
+            sum = a + b + carry;
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
 
-       return l1;
+        }
+        if (carry > 0) {
+            current.next = new ListNode(carry);
+        }
+        return result.next;
     }
 }
